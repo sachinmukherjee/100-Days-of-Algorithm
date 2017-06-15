93ec2b0b4b039c4d54294b5761cf0825689f3c00
sachinmukherjee <sachinmukherjee29@gmail.com>
2017-06-15 10:26:28 +0530

    Database file added

---
+++ database/OnlineAuction.sql
@@ -0,0 +1,57 @@
+create table if not exists Login
+(
+  login_id int(10) not null auto_increment,
+  email varchar(20) not null,
+  pass varchar(20) not null,
+  primary key(login_id,email)
+);
+
+
+create table if not exists Users
+(
+  
+  user_id int(10) not null auto_increment,
+  login_id int(10) not null references Login(Login),
+  profile_pic blob not null,
+  full_name varchar(30) not null,
+  email varchar(20) not null references Login(email),
+  city char(20) not null,
+  state char(20) not null,
+  gender char(2) not null,
+  phone_number long not null,
+  primary key(user_id)
+  );
+  
+  create table if not exists Product
+  (
+   product_id int(10) not null auto_increment,
+   product_name varchar(50) not null,
+   dat date not null,
+   tim time not null,
+   price int not null,
+   product_image blob not null,
+   userinfo int(10) not null references Users(user_id),
+   primary key(product_id)
+   );
+   
+   
+   create table if not exists Purchased
+   (
+     user_id int(10) not null references Users(user_id),
+     product_id int(10) not null references Product(product_id)
+   );
+
+   create table if not exists Saved
+   (
+   product_saved int(10) not null references Product(product_id),
+   user_saved int(10) not null references Users(user_id)
+   );
+   
+  
+    create table if not exists Bids
+   (
+    product_id int(10) not null references Product(product_id),
+    initial_price int not null,
+    final_price int not null,
+    user_id int(10) not null references User(user_id)
+    );
