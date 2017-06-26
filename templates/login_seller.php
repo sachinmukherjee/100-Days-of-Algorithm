76a504182458cbc88caf292a9c6c6b425f700f48
sachinmukherjee <sachinmukherjee29@gmail.com>
2017-06-26 16:59:23 +0530

    Seller Login styling added

--- templates/login_seller.php
+++ templates/login_seller.php
@@ -2,16 +2,25 @@
 <html>
 <head>
 	<title>Seller Login</title>
+	<link rel="stylesheet" type="text/css" href="css/sellerlogin.css">
 </head>
 <body>
+<div class="sellerlogin">
 <h2>Please Login </h2>
+<table>
 <form action = "seller_authentication.php" method="post">
-	<label>Username</label>
-	<input type="text" name="username"><span style="color:red;">* feild cannot be empty</span><br><br>
-	<label>Password</label>
-	<input type="password" name="password"><span style="color:red;">* feild cannot be empty</span><br><br>
-	<input type="submit" name="Login">
+<tr>
+	<td><label>Username</label></td>
+	<td><input type="text" name="username"></td><td><span style="color:red;">* feild cannot be empty</span></td>
+</tr>
+<tr>
+	<td><label>Password</label></td>
+	<td><input type="password" name="password"></td><td><span style="color:red;">* feild cannot be empty</span></td>
+</tr>
 </form>
+</table>
+	<button type="submit" name="Login">Login</button>
+</div>
 
 </body>
 </html>
