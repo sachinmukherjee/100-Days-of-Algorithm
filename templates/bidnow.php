f5ea7c88bf5900e16e75f7f34cf17d6a128521d7
sachinmukherjee <sachinmukherjee29@gmail.com>
2017-07-10 11:04:17 +0530

    selle login bootstrap styling added

--- templates/bidnow.php
+++ templates/bidnow.php
@@ -30,8 +30,15 @@ include 'database.php';
 <html>
 <head>
 	<title>Bidnow</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional Bootstrap theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container">
 <p><strong><?php echo $product_name;?></strong></p>
 <br><br>
 <p><strong><?php echo $price;?></strong></p>
@@ -43,8 +50,9 @@ include 'database.php';
       	echo "<strong>$final_price</strong>";
 ?>
 
-<form action = " bidnow.php" method="get">
-	<input type="text" name="mybid">
+<form action = " bidnow.php" method="get" class="form" role="form">
+<div class="form-group">
+	<input type="text" name="mybid" class="form-control">
 	<?php 
 	     if(isset($_GET['mybid']))
 	     {
@@ -67,9 +75,12 @@ include 'database.php';
 	     	}
 	     }
 ?>
+</div>
 </form>
+
+
 <br><br>
 <a href = "ongoing_auction.php">Click here to bid more</a>
-
+</div>
 </body>
 </html>
--- templates/login_seller.php
+++ templates/login_seller.php
@@ -2,23 +2,28 @@
 <html>
 <head>
 	<title>Seller Login</title>
-	<link rel="stylesheet" type="text/css" href="css/sellerlogin.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional Bootstrap theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="sellerlogin">
-<h2>Please Login </h2>
-<table>
-<form action = "seller_authentication.php" method="post">
-<tr>
-	<td><label>Username</label></td>
-	<td><input type="text" name="username"></td><td><span style="color:red;">* feild cannot be empty</span></td>
-</tr>
-<tr>
-	<td><label>Password</label></td>
-	<td><input type="password" name="password"></td><td><span style="color:red;">* feild cannot be empty</span></td>
-</tr>
-</table>
-	<input type="submit" name="Login">
+<div class="container">
+<h2 style="position: relative; top:1cm; left:1.5cm; font-weight: bolder;">Please Login </h2>
+
+<form action = "seller_authentication.php" method="post" class="form" role="form" style="position: relative; top:2cm;>
+<div class="form-group">
+	<label>Username</label>
+	<input type="text" name="username" class="form-control" style="width: 8cm;"><span style="color:red;">* feild cannot be empty</span>
+<div class="form-group">
+
+<div class="form-group">
+	<label>Password</label>
+	<input type="password" name="password" class="form-control" style="width: 8cm;"><span style="color:red;">* feild cannot be empty</span>
+</div>
+	<button type="submit" name="Login" class="btn btn-success">Login</button>
 </form>
 </div>
 
